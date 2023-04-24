bfa3e184cf4ec7e5634127248f3000d45fd5b4e3
Aamr-mohamed <110783762+Aamr-mohamed@users.noreply.github.com>
2023-04-24 14:03:31 +0200

    -u

--- app.js
+++ app.js
@@ -1,47 +1,60 @@
-function game(){                                                                     //the main game function
-    let playerOneScore=0;
-    let computerScore=0;
-    let gameContinues=true                                                           //The gameOver button ;)
-    console.log("Rock Paper Scissors")
-    while (gameContinues){                        
-        let computerSelection=getComputerChoice()                                    //Computer random input
-        let playerSelection=prompt("rock paper scissors").toLowerCase();             //The player one input
-        if (playerSelection==computerSelection){                                     //The game rules
-            console.log("its a draw")
-        }else if (playerSelection =="rock"){
-            if (computerSelection=="scissors"){
-                console.log("player one won this round")
-                playerOneScore+=1
-            }else{
-                console.log("computer won this round")
-                computerScore+=1
-            }
-        }else if (playerSelection=="scissors"){
-            if (computerSelection=="rock"){
-                console.log("computer won this round")
-                computerScore+=1
-            }else {
-                console.log("player one won this round")
-                playerOneScore+=1
-            }
-        }else if (playerSelection=="paper"){
-            if (computerSelection=="rock"){
-                console.log("computer won this round")
-                computerScore+=1
-            }else{
-                console.log("player one won this round")
-            }
-            }
-        if (playerOneScore>=5){
-            gameContinues=false
-            console.log("Congrates player one won the game")
-        }else if (computerScore>=5){
-            gameContinues=false
-            console.log("computer win the game ...!... ")
-        }    
+const keys=document.querySelectorAll('button');                                                      
+let playerOneScore=0;
+let computerScore=0;    
+let result =""                                                       
+console.log("Rock Paper Scissors")
+function disableButtons() {
+    keys.forEach(elem => {
+        elem.disabled = true
+    })
+}
+function playRound(playerSelection){
+    let computerSelection=getComputerChoice()                  
+    if (playerOneScore==5){
+        result="Congrates player one won the game"
+        console.log(result)
+        return disableButtons()
+    }else if (computerScore==5){
+        result="computer win the game ...!... "
+        console.log(result)
+        return disableButtons()
+    }                                                                     
+    
+    if (playerSelection==computerSelection){                                     
+        console.log("its a draw")
+    }else if (playerSelection =="rock"){
+        if (computerSelection=="scissors"){
+            playerOneScore+=1
+            result=("you win"+playerSelection+"beats"+computerSelection+"The score is"+playerOneScore+"comp"+computerScore)
+            console.log(result)
+        }else{
+            console.log("computer won this round")
+            computerScore+=1
+        }
+    }else if (playerSelection=="scissors"){
+        if (computerSelection=="rock"){
+            console.log("computer won this round")
+            computerScore+=1
+        }else {
+            playerOneScore+=1
+            result=("you win "+ playerSelection +" beats "+ computerSelection +" The score is "+ playerOneScore +" comp "+ computerScore )
+            console.log(result)
+
+        }
+    }else if (playerSelection=="paper"){
+        if (computerSelection=="rock"){
+            console.log("computer won this round")
+            computerScore+=1
+        }else{
+            console.log("player one won this round")
+        }
+    }
+    document.getElementById('result').textContent = result
+    return
+                
         
     
-    }
+}
     /*function getComputerChoice(){                                                 //Computer random input function
         let computerChoice=Math.floor(Math.random()*10)+1;
         if (computerChoice>7){
@@ -51,8 +64,26 @@ function game(){                                                                
         }else
             return ('scissors')
         }*/
-    function getComputerChoice(){
+function getComputerChoice(){
         const choices=['rock','paper','scissors']
         return choices[Math.floor(Math.random()*choices.length)];
-    }
 }
+keys.forEach(key=>{
+    key.addEventListener('click', function(){
+        playRound(key.value)
+    })
+})
+/*const keys1=document.querySelector("#btn1");
+keys1.addEventListener('click',()=>{return("rock")});
+const keys2=document.querySelector("#btn2");
+keys2.addEventListener('click',()=>{return ("paper")});
+const keys3=document.querySelector("#btn3");
+keys3.addEventListener('click',()=>{return ("scissors")});*/    
+//key.addEventListener('click',()=>{return ("rock")})
+    
+    
+
+
+
+    
+
